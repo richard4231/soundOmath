ccd7ae6b9200ad403330b54b7c8b7a5c1e9c5d4a
Martin Guggisberg <martin.guggisberg@gmail.com>
2016-01-23 00:09:38 +0100

    some buttons

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -3,6 +3,36 @@
 
 // const g = 9.81;
 
-a = 23; 
+// Register Listeners
 
-let b =  (x,y) => x+y;
+$(document).ready(function(){
+
+    
+
+ //    $('#btn-row1-1').on('click', () => {
+ //    var $btn = $(this).button('loading')
+   
+ //    $btn.button('reset')
+	// });
+
+// Listen on Menu entry
+	let idArr = ['#btn-row1-1','#btn-row1-2','#btn-row1-3','#btn-row1-4'];
+    for (let i in idArr) {
+    	alert(idArr[i]);
+    	$(idArr[i])
+			.parent()
+			.children('ul.dropdown-menu')
+			.on('click', (e) => {
+				$(idArr[i])
+				.parent()
+				.parent()
+				.children('input.form-control')
+				.attr('value',e.target.text);
+		});	
+    }
+
+
+});
+
+
+
