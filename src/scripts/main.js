991d070baa97a419b9dea033eee936408ff6c7e0
Martin Guggisberg <martin.guggisberg@gmail.com>
2016-01-24 21:26:35 +0100

    draw

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,7 +18,6 @@ $(document).ready(function(){
 // Listen on Menu entry
 	let idArr = ['#btn-row1-1','#btn-row1-2','#btn-row1-3','#btn-row1-4'];
     for (let i in idArr) {
-    	alert(idArr[i]);
     	$(idArr[i])
 			.parent()
 			.children('ul.dropdown-menu')
@@ -30,6 +29,43 @@ $(document).ready(function(){
 				.attr('value',e.target.text);
 		});	
     }
+
+	// Draw so stuff
+	var width = 700,
+	    height = 400,
+	    div = d3.select('#chart'),
+	    svg = div.append('svg')
+	        .attr('width', width)
+	        .attr('height', height),
+	    rw = 20,
+	    rh = 20;
+
+	var data = [];
+	for (var k = 0; k < 4; k += 1) {
+	    data.push(d3.range(28));
+	}
+
+
+	// Create a group for each row in the data matrix and
+	// translate the group vertically
+	var grp = svg.selectAll('g')
+	    .data(data)
+	    .enter()
+	    .append('g')
+	    .attr('transform', function(d, i) {
+	        return 'translate(0, ' + 24 * i + ')';
+	    });
+
+	// For each group, create a set of rectangles and bind 
+	// them to the inner array (the inner array is already
+	// binded to the group)
+	grp.selectAll('rect')
+	    .data(function(d) { return d; })
+	    .enter()
+	    .append('rect')
+	        .attr('x', function(d, i) { return 28 * i; })
+	        .attr('width', rw)
+	        .attr('height', rh);
 
 
 });
